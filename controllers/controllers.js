672273b870474d096967082a9a96f64519120222
Deeppatel-4032 <deeppatel7868@gmail.com>
2024-08-14 15:55:35 +0530

    read(view)

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -2,21 +2,33 @@
 let storageTodo = [];
 
 const defaultCon = (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { todos : storageTodo });
 }
 
 const todoCon = (req, res) => {
     console.log(req.body);
-    
+
     const todoListObj = {
         id : storageTodo.length + 1,
         todoList : req.body.todoList
     }
-    
+
     storageTodo = [...storageTodo, todoListObj]
     console.log("todoListObj", todoListObj);
+    
     
     res.redirect("/");
 }
 
-module.exports = { defaultCon, todoCon }
+const editCon = (req, res) => {
+    const id = req.params.id;
+    const editSingleRecod =  storageTodo.find((editId) => {
+        return editId.id == id;
+    })
+
+    res.render("edite_todo", { editSingleRecod });
+    console.log("editSingleRecod", editSingleRecod);
+}
+
+
+module.exports = { defaultCon, todoCon, editCon }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const con = require("../controllers/controllers.js");
 
 router.get("/", con.defaultCon);
 router.post("/todoCon", con.todoCon)
+router.get("/editCon/:id", con.editCon)
 
 module.exports = router;
